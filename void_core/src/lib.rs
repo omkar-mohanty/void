36949a0f2a1ec5e3d7ce6e9370593ea0a4924afa
omkar-mohanty <franzohouser@gmail.com>
2024-03-20 15:22:04 +0530

    Rendering engine

--- void_core/src/lib.rs
+++ void_core/src/lib.rs
@@ -1,14 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::result::Result;
+use thiserror::Error;
+
+pub trait Event: Sized {}
+
+pub trait System: 'static {
+    type T: Event + 'static;
+    fn process_event(&mut self, event: Self::T) -> Result<(), Error>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("System Error `{0}`")]
+    System(String),
+    #[error("Resource Error `{0}`")]
+    Resource(String),
 }
